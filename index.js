1df06d8f783b2263dba79c8afcb42825b04f2206
Joris Hens <joris@goodbytes.be>
2019-04-15 16:29:04 +0200

    Routers are now external

--- index.js
+++ index.js
@@ -2,29 +2,9 @@ const express = require('express')
 const logger = require('./middleware/logger')
 const app = express()
 const port = 3000
+const apiV1Todosrouter = require('./routers/api/v1/todos')
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/api/v1/todos', (req, res) => {
-    res.send("GET todos");
-});
-
-app.post('/api/v1/todos', (req, res) => {
-    res.send("POST todos");
-});
-
-app.put('/api/v1/todos/:id', (req, res) => {
-    res.send("PUT todos");
-});
-
-app.delete('/api/v1/todos/:id', (req, res) => {
-    res.send("DELETE todo " + req.params.id);
-});
-
-// GET  api/v1/todos
-// POST api/v1/todos
-// PUT  api/v1/todos/3
-// DEL  api/v1/todos/3
-
+app.use('/api/v1/todos', apiV1Todosrouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routers/api/v1/todos.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("GET todos");
+});
+
+router.post('/', (req, res) => {
+    res.send("POST todos");
+});
+
+router.put('/:id', (req, res) => {
+    res.send("PUT todos");
+});
+
+router.delete('/:id', (req, res) => {
+    res.send("DELETE todo " + req.params.id);
+});
+
+module.exports = router;
