3315a626ce23e638b2d26512e7dafdfb9c6a8df4
Joris Hens <joris@goodbytes.be>
2019-04-16 10:38:34 +0200

    Add mongoose

--- controllers/api/v1/todos.js
+++ controllers/api/v1/todos.js
@@ -1,21 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const todoSchema = new Schema({
+    text: String,
+    user: String,
+    completed: Boolean
+})
+const Todo = mongoose.model('Todo', todoSchema);
+
 const getAll = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todos": []
+    Todo.find({
+        "user": "Joris"
+    }, (err, docs) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "todos": docs
+                }
+            });
         }
     });
+
+
 }
 
 const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todo": {
-                "text": "Learn Node.js"
-            }
+    let todo = new Todo();
+    todo.text = "My first todo";
+    todo.user = "Joris";
+    todo.completed = false;
+    todo.save((err, doc) => {
+        if (!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "todo": doc
+                }
+            });
         }
-    });
+    })
+
+
 }
 
 module.exports.getAll = getAll;
