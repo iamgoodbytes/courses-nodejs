b2f2dffad3eccde084f4a69f164c9842e5477ac1
Joris Hens <joris@goodbytes.be>
2019-04-16 16:46:35 +0200

    Add webtokens authentication and strategy

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,7 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res, next) => {
-    console.log(req.body);
-
     let username = req.body.username; // UI of postman
     let password = req.body.password;
 
@@ -11,8 +10,18 @@ const signup = async (req, res, next) => {
     });
     await user.setPassword(password);
     await user.save().then(result => {
+        console.log(result);
+
+        let token = jwt.sign({
+            uid: result._id,
+            username: result.username
+        }, "MyVerySecretWord");
+
         res.json({
-            "status": "success"
+            "status": "success",
+            "data": {
+                "token": token
+            }
         })
     }).catch(error => {
         res.json({
