4e027bc196c8079bb84883a7f716d26d3d67fe00
Joris Hens <joris@goodbytes.be>
2019-04-17 11:06:58 +0200

    Added comment

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const config = require('config');
 
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
+// load dbconn from process.env
 mongoose.connect(process.env.dbconn || config.get('Database.conn'), {
   useNewUrlParser: true
 });
