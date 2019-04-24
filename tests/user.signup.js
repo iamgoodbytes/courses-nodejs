3de85314c0c2d3872bdc8699118cec8e643eeac2
Joris Hens <joris@goodbytes.be>
2019-04-24 14:49:21 +0200

    Re-wrote tests for video tutorial

---
+++ tests/user.signup.js
@@ -0,0 +1,38 @@
+import test from 'ava'
+import request from 'supertest'
+import MongodbMemoryServer from 'mongodb-memory-server'
+
+test.before(async (t) => {
+    // This runs before all tests
+    const mongod = new MongodbMemoryServer({
+        instance: {
+            port: 37001,
+            ip: "127.0.0.1",
+            dbName: "todoTEST"
+        }
+    });
+
+    const uri = await mongod.getConnectionString();
+    console.log("🔥" + uri);
+    const app = require('../app');
+
+    t.context.app = app;
+    t.context.mongod = mongod;
+
+});
+
+test.serial('User signup', async t => {
+    let response = await request(t.context.app)
+        .post('/users/signup')
+        .send({
+            "username": "GoodBytes",
+            "password": "this is not safe"
+        });
+
+    t.is(response.status, 200);
+    t.is(response.body.status, "success");
+});
+
+test.after.always(t => {
+    t.context.mongod.stop();
+});
