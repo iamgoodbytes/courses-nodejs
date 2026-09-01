8a20e5e13f16511af60cb81888de30e159d7ff00
iamgoodbytes <joris@goodbytes.be>
2026-09-01 14:33:06 +0200

    feat: add HTTP server implementation in index.mjs and update package.json scripts

--- index.js
+++
@@ -1,9 +0,0 @@
-const cowsay = require("cowsay");
-
-console.log(
-    cowsay.say({
-        text: "I'm a moooodule",
-        e: "oO",
-        T: "X ",
-    }),
-);
---
+++ index.mjs
@@ -0,0 +1,21 @@
+// server.mjs
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+    console.log(req.url);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    switch (req.url) {
+        case "/signup":
+            res.end("Signup page!");
+            break;
+        default:
+            res.end("Party people ✌🏼");
+    }
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, "127.0.0.1", () => {
+    console.log("Listening on 127.0.0.1:3000");
+});
+
+// run with `node server.mjs`
