c600f0922fa701b0759bf66f02a3215981f1409e
iamgoodbytes <joris@goodbytes.be>
2026-09-01 14:48:42 +0200

    feat: express routing

--- index.mjs
+++ index.mjs
@@ -1,21 +1,24 @@
-// server.mjs
-import { createServer } from "node:http";
+import express from "express";
 
-const server = createServer((req, res) => {
-    console.log(req.url);
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    switch (req.url) {
-        case "/signup":
-            res.end("Signup page!");
-            break;
-        default:
-            res.end("Party people ✌🏼");
-    }
+const app = express();
+const port = 3000;
+
+app.get("/api/v1/todos", (req, res) => {
+    res.send("GET todos");
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, "127.0.0.1", () => {
-    console.log("Listening on 127.0.0.1:3000");
+app.post("/api/v1/todos", (req, res) => {
+    res.send("POST todos");
 });
 
-// run with `node server.mjs`
+app.put("/api/v1/todos/:id", (req, res) => {
+    res.send("PUT todos");
+});
+
+app.delete("/api/v1/todos/:id", (req, res) => {
+    res.send("DEL todos width id: " + req.params.id);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
