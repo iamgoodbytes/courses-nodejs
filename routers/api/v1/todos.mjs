150f0bf33bdba475459f8c9b6a40776bfab98717
iamgoodbytes <joris@goodbytes.be>
2026-09-02 10:04:57 +0200

    feat: controllers structure

--- routers/api/v1/todos.mjs
+++ routers/api/v1/todos.mjs
@@ -1,20 +1,16 @@
 import { Router } from "express";
+import {
+    list,
+    create,
+    update,
+    remove,
+} from "../../../controllers/api/v1/todos.mjs";
+
 const router = Router();
 
-router.get("/api/v1/todos", (req, res) => {
-    res.send("GET todos");
-});
-
-router.post("/api/v1/todos", (req, res) => {
-    res.send("POST todos");
-});
-
-router.put("/api/v1/todos/:id", (req, res) => {
-    res.send("PUT todos");
-});
-
-router.delete("/api/v1/todos/:id", (req, res) => {
-    res.send("DEL todos width id: " + req.params.id);
-});
+router.get("/api/v1/todos", list);
+router.post("/api/v1/todos", create);
+router.put("/api/v1/todos/:id", update);
+router.delete("/api/v1/todos/:id", remove);
 
 export default router;
